f7f764cb8b7a63a97efff82248c23cfb5bb51d67
mitchellnel <mitchellmclin@gmail.com>
2022-07-24 10:39:57 +0800

    Scaffolded the set-name message with Ignite CLI

--- vue/src/store/generated/nameservice-nel/nameservicenel.nameservice/module/index.ts
+++ vue/src/store/generated/nameservice-nel/nameservicenel.nameservice/module/index.ts
@@ -4,10 +4,12 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgSetName } from "./types/nameservice/tx";
 import { MsgBuyName } from "./types/nameservice/tx";
 
 
 const types = [
+  ["/nameservicenel.nameservice.MsgSetName", MsgSetName],
   ["/nameservicenel.nameservice.MsgBuyName", MsgBuyName],
   
 ];
@@ -41,6 +43,7 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgSetName: (data: MsgSetName): EncodeObject => ({ typeUrl: "/nameservicenel.nameservice.MsgSetName", value: MsgSetName.fromPartial( data ) }),
     msgBuyName: (data: MsgBuyName): EncodeObject => ({ typeUrl: "/nameservicenel.nameservice.MsgBuyName", value: MsgBuyName.fromPartial( data ) }),
     
   };
