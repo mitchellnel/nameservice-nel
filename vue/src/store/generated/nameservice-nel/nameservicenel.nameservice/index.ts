a9e87c0084ff569842c26895dec4f429de7a5042
mitchellnel <mitchellmclin@gmail.com>
2022-07-24 10:40:58 +0800

    Scaffolded the delete-name message for the nameservice module using Ignite CLI

--- vue/src/store/generated/nameservice-nel/nameservicenel.nameservice/index.ts
+++ vue/src/store/generated/nameservice-nel/nameservicenel.nameservice/index.ts
@@ -135,21 +135,6 @@ export default {
 		},
 		
 		
-		async sendMsgSetName({ rootGetters }, { value, fee = [], memo = '' }) {
-			try {
-				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgSetName(value)
-				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
-	gas: "200000" }, memo})
-				return result
-			} catch (e) {
-				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgSetName:Init Could not initialize signing client. Wallet is required.')
-				}else{
-					throw new Error('TxClient:MsgSetName:Send Could not broadcast Tx: '+ e.message)
-				}
-			}
-		},
 		async sendMsgBuyName({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -165,20 +150,37 @@ export default {
 				}
 			}
 		},
-		
-		async MsgSetName({ rootGetters }, { value }) {
+		async sendMsgDeleteName({ rootGetters }, { value, fee = [], memo = '' }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
-				const msg = await txClient.msgSetName(value)
-				return msg
+				const msg = await txClient.msgDeleteName(value)
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
 			} catch (e) {
 				if (e == MissingWalletError) {
-					throw new Error('TxClient:MsgSetName:Init Could not initialize signing client. Wallet is required.')
-				} else{
-					throw new Error('TxClient:MsgSetName:Create Could not create message: ' + e.message)
+					throw new Error('TxClient:MsgDeleteName:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgDeleteName:Send Could not broadcast Tx: '+ e.message)
 				}
 			}
 		},
+		async sendMsgSetName({ rootGetters }, { value, fee = [], memo = '' }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgSetName(value)
+				const result = await txClient.signAndBroadcast([msg], {fee: { amount: fee, 
+	gas: "200000" }, memo})
+				return result
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgSetName:Init Could not initialize signing client. Wallet is required.')
+				}else{
+					throw new Error('TxClient:MsgSetName:Send Could not broadcast Tx: '+ e.message)
+				}
+			}
+		},
+		
 		async MsgBuyName({ rootGetters }, { value }) {
 			try {
 				const txClient=await initTxClient(rootGetters)
@@ -189,6 +191,32 @@ export default {
 					throw new Error('TxClient:MsgBuyName:Init Could not initialize signing client. Wallet is required.')
 				} else{
 					throw new Error('TxClient:MsgBuyName:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		async MsgDeleteName({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgDeleteName(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgDeleteName:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgDeleteName:Create Could not create message: ' + e.message)
+				}
+			}
+		},
+		async MsgSetName({ rootGetters }, { value }) {
+			try {
+				const txClient=await initTxClient(rootGetters)
+				const msg = await txClient.msgSetName(value)
+				return msg
+			} catch (e) {
+				if (e == MissingWalletError) {
+					throw new Error('TxClient:MsgSetName:Init Could not initialize signing client. Wallet is required.')
+				} else{
+					throw new Error('TxClient:MsgSetName:Create Could not create message: ' + e.message)
 				}
 			}
 		},
--- x/nameservice/types/tx.pb.go
+++ x/nameservice/types/tx.pb.go
@@ -219,17 +219,107 @@ func (m *MsgSetNameResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_MsgSetNameResponse proto.InternalMessageInfo
 
+type MsgDeleteName struct {
+	Creator string `protobuf:"bytes,1,opt,name=creator,proto3" json:"creator,omitempty"`
+	Name    string `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty"`
+}
+
+func (m *MsgDeleteName) Reset()         { *m = MsgDeleteName{} }
+func (m *MsgDeleteName) String() string { return proto.CompactTextString(m) }
+func (*MsgDeleteName) ProtoMessage()    {}
+func (*MsgDeleteName) Descriptor() ([]byte, []int) {
+	return fileDescriptor_df4dec9b515ec245, []int{4}
+}
+func (m *MsgDeleteName) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgDeleteName) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgDeleteName.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgDeleteName) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgDeleteName.Merge(m, src)
+}
+func (m *MsgDeleteName) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgDeleteName) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgDeleteName.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgDeleteName proto.InternalMessageInfo
+
+func (m *MsgDeleteName) GetCreator() string {
+	if m != nil {
+		return m.Creator
+	}
+	return ""
+}
+
+func (m *MsgDeleteName) GetName() string {
+	if m != nil {
+		return m.Name
+	}
+	return ""
+}
+
+type MsgDeleteNameResponse struct {
+}
+
+func (m *MsgDeleteNameResponse) Reset()         { *m = MsgDeleteNameResponse{} }
+func (m *MsgDeleteNameResponse) String() string { return proto.CompactTextString(m) }
+func (*MsgDeleteNameResponse) ProtoMessage()    {}
+func (*MsgDeleteNameResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_df4dec9b515ec245, []int{5}
+}
+func (m *MsgDeleteNameResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgDeleteNameResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgDeleteNameResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgDeleteNameResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgDeleteNameResponse.Merge(m, src)
+}
+func (m *MsgDeleteNameResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgDeleteNameResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgDeleteNameResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgDeleteNameResponse proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*MsgBuyName)(nil), "nameservicenel.nameservice.MsgBuyName")
 	proto.RegisterType((*MsgBuyNameResponse)(nil), "nameservicenel.nameservice.MsgBuyNameResponse")
 	proto.RegisterType((*MsgSetName)(nil), "nameservicenel.nameservice.MsgSetName")
 	proto.RegisterType((*MsgSetNameResponse)(nil), "nameservicenel.nameservice.MsgSetNameResponse")
+	proto.RegisterType((*MsgDeleteName)(nil), "nameservicenel.nameservice.MsgDeleteName")
+	proto.RegisterType((*MsgDeleteNameResponse)(nil), "nameservicenel.nameservice.MsgDeleteNameResponse")
 }
 
 func init() { proto.RegisterFile("nameservice/tx.proto", fileDescriptor_df4dec9b515ec245) }
 
 var fileDescriptor_df4dec9b515ec245 = []byte{
-	// 249 bytes of a gzipped FileDescriptorProto
+	// 290 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0xc9, 0x4b, 0xcc, 0x4d,
 	0x2d, 0x4e, 0x2d, 0x2a, 0xcb, 0x4c, 0x4e, 0xd5, 0x2f, 0xa9, 0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9,
 	0x17, 0x92, 0x42, 0x12, 0xcd, 0x4b, 0xcd, 0xd1, 0x43, 0xe2, 0x2a, 0xf9, 0x70, 0x71, 0xf9, 0x16,
@@ -238,14 +328,17 @@ var fileDescriptor_df4dec9b515ec245 = []byte{
 	0x20, 0x6d, 0x12, 0x4c, 0x60, 0x61, 0x30, 0x5b, 0x48, 0x80, 0x8b, 0x39, 0x29, 0x33, 0x45, 0x82,
 	0x19, 0x2c, 0x04, 0x62, 0x2a, 0x89, 0x70, 0x09, 0x21, 0x4c, 0x0b, 0x4a, 0x2d, 0x2e, 0xc8, 0xcf,
 	0x2b, 0x4e, 0x55, 0x0a, 0x00, 0xdb, 0x11, 0x9c, 0x5a, 0x42, 0x86, 0x1d, 0x22, 0x5c, 0xac, 0x65,
-	0x89, 0x39, 0xa5, 0xa9, 0x50, 0x5b, 0x20, 0x1c, 0xa8, 0x3d, 0x50, 0x13, 0x61, 0xf6, 0x18, 0x9d,
-	0x66, 0xe4, 0x62, 0xf6, 0x2d, 0x4e, 0x17, 0x4a, 0xe4, 0x62, 0x87, 0x79, 0x48, 0x4d, 0x0f, 0xb7,
-	0xdf, 0xf5, 0x10, 0x4e, 0x95, 0xd2, 0x23, 0x4e, 0x1d, 0xcc, 0x2a, 0x90, 0x15, 0x30, 0xff, 0x10,
-	0xb2, 0x02, 0xaa, 0x8e, 0xa0, 0x15, 0x68, 0xbe, 0x71, 0xb2, 0x3d, 0xf1, 0x48, 0x8e, 0xf1, 0xc2,
-	0x23, 0x39, 0xc6, 0x07, 0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0xb8, 0xf0, 0x58, 0x8e, 0xe1,
-	0xc6, 0x63, 0x39, 0x86, 0x28, 0x65, 0x24, 0x9d, 0xba, 0x79, 0xa9, 0x39, 0xfa, 0x15, 0xfa, 0x28,
-	0xf1, 0x5e, 0x59, 0x90, 0x5a, 0x9c, 0xc4, 0x06, 0x8e, 0x7b, 0x63, 0x40, 0x00, 0x00, 0x00, 0xff,
-	0xff, 0xf3, 0x0e, 0xa9, 0xa5, 0x13, 0x02, 0x00, 0x00,
+	0x89, 0x39, 0xa5, 0xa9, 0x50, 0x5b, 0x20, 0x1c, 0xa8, 0x3d, 0x50, 0x13, 0xe1, 0xf6, 0xd8, 0x72,
+	0xf1, 0xfa, 0x16, 0xa7, 0xbb, 0xa4, 0xe6, 0xa4, 0x96, 0xa4, 0x92, 0x6e, 0x95, 0x92, 0x38, 0x97,
+	0x28, 0x8a, 0x76, 0x98, 0xb9, 0x46, 0xdb, 0x99, 0xb8, 0x98, 0x7d, 0x8b, 0xd3, 0x85, 0x12, 0xb9,
+	0xd8, 0x61, 0x01, 0xa5, 0xa6, 0x87, 0x3b, 0x4c, 0xf5, 0x10, 0x41, 0x20, 0xa5, 0x47, 0x9c, 0x3a,
+	0x98, 0x55, 0x20, 0x2b, 0x60, 0xe1, 0x44, 0xc8, 0x0a, 0xa8, 0x3a, 0x82, 0x56, 0xa0, 0x85, 0x92,
+	0x50, 0x16, 0x17, 0x17, 0x52, 0x10, 0x69, 0x12, 0xd0, 0x8d, 0x50, 0x2a, 0x65, 0x48, 0xb4, 0x52,
+	0x98, 0x5d, 0x4e, 0xb6, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c,
+	0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78, 0x2c, 0xc7, 0x10, 0xa5, 0x8c,
+	0xa4, 0x59, 0x37, 0x2f, 0x35, 0x47, 0xbf, 0x42, 0x1f, 0x25, 0xed, 0x56, 0x16, 0xa4, 0x16, 0x27,
+	0xb1, 0x81, 0xd3, 0xaf, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0xcf, 0x94, 0x7c, 0x3b, 0xd7, 0x02,
+	0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -262,6 +355,7 @@ const _ = grpc.SupportPackageIsVersion4
 type MsgClient interface {
 	BuyName(ctx context.Context, in *MsgBuyName, opts ...grpc.CallOption) (*MsgBuyNameResponse, error)
 	SetName(ctx context.Context, in *MsgSetName, opts ...grpc.CallOption) (*MsgSetNameResponse, error)
+	DeleteName(ctx context.Context, in *MsgDeleteName, opts ...grpc.CallOption) (*MsgDeleteNameResponse, error)
 }
 
 type msgClient struct {
@@ -290,10 +384,20 @@ func (c *msgClient) SetName(ctx context.Context, in *MsgSetName, opts ...grpc.Ca
 	return out, nil
 }
 
+func (c *msgClient) DeleteName(ctx context.Context, in *MsgDeleteName, opts ...grpc.CallOption) (*MsgDeleteNameResponse, error) {
+	out := new(MsgDeleteNameResponse)
+	err := c.cc.Invoke(ctx, "/nameservicenel.nameservice.Msg/DeleteName", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // MsgServer is the server API for Msg service.
 type MsgServer interface {
 	BuyName(context.Context, *MsgBuyName) (*MsgBuyNameResponse, error)
 	SetName(context.Context, *MsgSetName) (*MsgSetNameResponse, error)
+	DeleteName(context.Context, *MsgDeleteName) (*MsgDeleteNameResponse, error)
 }
 
 // UnimplementedMsgServer can be embedded to have forward compatible implementations.
@@ -305,6 +409,9 @@ func (*UnimplementedMsgServer) BuyName(ctx context.Context, req *MsgBuyName) (*M
 }
 func (*UnimplementedMsgServer) SetName(ctx context.Context, req *MsgSetName) (*MsgSetNameResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method SetName not implemented")
+}
+func (*UnimplementedMsgServer) DeleteName(ctx context.Context, req *MsgDeleteName) (*MsgDeleteNameResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteName not implemented")
 }
 
 func RegisterMsgServer(s grpc1.Server, srv MsgServer) {
@@ -347,6 +454,24 @@ func _Msg_SetName_Handler(srv interface{}, ctx context.Context, dec func(interfa
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Msg_DeleteName_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MsgDeleteName)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MsgServer).DeleteName(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/nameservicenel.nameservice.Msg/DeleteName",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MsgServer).DeleteName(ctx, req.(*MsgDeleteName))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "nameservicenel.nameservice.Msg",
 	HandlerType: (*MsgServer)(nil),
@@ -358,6 +483,10 @@ var _Msg_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "SetName",
 			Handler:    _Msg_SetName_Handler,
+		},
+		{
+			MethodName: "DeleteName",
+			Handler:    _Msg_DeleteName_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -498,6 +627,66 @@ func (m *MsgSetNameResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *MsgDeleteName) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgDeleteName) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgDeleteName) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Name) > 0 {
+		i -= len(m.Name)
+		copy(dAtA[i:], m.Name)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.Name)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.Creator) > 0 {
+		i -= len(m.Creator)
+		copy(dAtA[i:], m.Creator)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.Creator)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *MsgDeleteNameResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgDeleteNameResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgDeleteNameResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintTx(dAtA []byte, offset int, v uint64) int {
 	offset -= sovTx(v)
 	base := offset
@@ -561,6 +750,32 @@ func (m *MsgSetName) Size() (n int) {
 }
 
 func (m *MsgSetNameResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgDeleteName) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Creator)
+	if l > 0 {
+		n += 1 + l + sovTx(uint64(l))
+	}
+	l = len(m.Name)
+	if l > 0 {
+		n += 1 + l + sovTx(uint64(l))
+	}
+	return n
+}
+
+func (m *MsgDeleteNameResponse) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -944,6 +1159,170 @@ func (m *MsgSetNameResponse) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: MsgSetNameResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgDeleteName) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgDeleteName: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgDeleteName: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Creator", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Creator = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Name", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Name = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgDeleteNameResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgDeleteNameResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgDeleteNameResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
